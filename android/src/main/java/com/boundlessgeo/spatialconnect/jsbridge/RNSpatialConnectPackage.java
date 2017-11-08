e2731e7d4ceb711502aa2fe7814b665c2ee74562
Marc J Cenac <mcenac@boundlessgeo.com>
2017-11-07 18:16:42 -0600

    update react packages to latest releases

--- android/src/main/java/com/boundlessgeo/spatialconnect/jsbridge/RNSpatialConnectPackage.java
+++ android/src/main/java/com/boundlessgeo/spatialconnect/jsbridge/RNSpatialConnectPackage.java
@@ -27,11 +27,6 @@ import java.util.List;
 public class RNSpatialConnectPackage implements ReactPackage {
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
